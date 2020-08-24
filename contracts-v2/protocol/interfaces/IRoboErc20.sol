6249ecb9049b95fab8a1fa4b3f94d607c32878a2
Devan Purhar <devan.p@shaw.ca>
2020-08-24 17:57:28 -0400

    add erc20 interface to RoboTokens

--- contracts-v2/protocol/interfaces/IRoboErc20.sol
+++ contracts-v2/protocol/interfaces/IRoboErc20.sol
@@ -25,7 +25,7 @@ pragma solidity 0.5.11;
 /// @title   IRoboErc20
 ///
 /// @notice  Interface of the RoboErc20 contract, which is used for RAY tokens that
-///          represent ERC-20 asset positions.
+///          represent ERC-20 asset positions. RoboErc20 are ERC-20 standard compliant.
 ///
 /// @dev     VERSION: 1.0
 ///          AUTHOR: Devan Purhar
@@ -106,6 +106,71 @@ interface IRoboErc20 {
   ///
   /// @return  The burn exchange rate between the RAY tokens and the underlying.
   function exchangeRateBurn() external returns (uint);
+
+
+  // --------------------- STANDARD ERC-20 INTERFACE --------------------- //
+
+
+  /// @dev Returns the amount of tokens in existence.
+  function totalSupply() external view returns (uint256);
+
+
+  /// @dev Returns the amount of tokens owned by `account`.
+  function balanceOf(address account) external view returns (uint256);
+
+
+  /// @dev Moves `amount` tokens from the caller's account to `recipient`.
+  ///
+  /// Returns a boolean value indicating whether the operation succeeded.
+  ///
+  /// Emits a {Transfer} event.
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+
+  /// @dev Returns the remaining number of tokens that `spender` will be
+  /// allowed to spend on behalf of `owner` through {transferFrom}. This is
+  /// zero by default.
+  ///
+  /// This value changes when {approve} or {transferFrom} are called.
+  function allowance(address owner, address spender) external view returns (uint256);
+
+
+  /// @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+  ///
+  /// Returns a boolean value indicating whether the operation succeeded.
+  ///
+  /// IMPORTANT: Beware that changing an allowance with this method brings the risk
+  /// that someone may use both the old and the new allowance by unfortunate
+  /// transaction ordering. One possible solution to mitigate this race
+  /// condition is to first reduce the spender's allowance to 0 and set the
+  /// desired value afterwards:
+  /// https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+  ///
+  /// Emits an {Approval} event.
+  function approve(address spender, uint256 amount) external returns (bool);
+
+
+  /// @dev Moves `amount` tokens from `sender` to `recipient` using the
+  /// allowance mechanism. `amount` is then deducted from the caller's
+  /// allowance.
+  ///
+  /// Returns a boolean value indicating whether the operation succeeded.
+  ///
+  /// Emits a {Transfer} event.
+  function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+
+  /// @dev Emitted when `value` tokens are moved from one account (`from`) to
+  /// another (`to`).
+  ///
+  /// Note that `value` may be zero.
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+
+  /// @dev Emitted when the allowance of a `spender` for an `owner` is set by
+  /// a call to {approve}. `value` is the new allowance.
+  ///
+  event Approval(address indexed owner, address indexed spender, uint256 value);
 
 
 }
